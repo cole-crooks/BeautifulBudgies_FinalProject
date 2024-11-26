579e9fc747315cd665ef18e4b9875e37ad95637d
Cole Crooks <crookscl@mail.uc.edu>
2024-11-26 13:51:39 -0500

    Recent Changes as of 11/26/2024 13:51

--- crookscl_FinalProject/index.aspx.cs
+++ crookscl_FinalProject/index.aspx.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+* Name: Cole Crooks
+* email:  crookscl@mail.uc.edu
+* Assignment Number: Final Project
+* Due Date:   12/10/2024
+* Course #/Section:   IS3050-001
+* Semester/Year:   Fall 2024
+* Brief Description of the assignment:  Final Project for the IS 3050 Web Development w/ .NET
+
+* Brief Description of what this module does. Demonstrating all that we've learned this year, solve one (1) hard leetcode problem per person with class knowledge and Github
+* Citations:https://leetcode.com/problems/parsing-a-boolean-expression/description/
+            https://github.com/cole-crooks/crookscl_FinalProject          
+* Anything else that's relevant: My collaborators on this project are Leah Radcliffe (radclilr@mail.uc.edu), Peter Phan (phanpv@mail.uc.edu), and Peyton Brock (brockps@mail.uc.edu)
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
