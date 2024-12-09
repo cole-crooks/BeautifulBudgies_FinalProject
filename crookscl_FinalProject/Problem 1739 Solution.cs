a850ee5bbbce88caf15df594a6f6df9d0f355265
daqdaniels <daq1.daniels@gmail.com>
2024-12-09 01:26:17 -0500

    Revised: I did question 3 problem 1739

--- crookscl_FinalProject/Problem 1739 Solution.cs
+++ crookscl_FinalProject/Problem 1739 Solution.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+* Name: Daquan Daniels
+* Email: Danieldu@mail.uc.edu
+* Assignment Number: Final Project
+* Due Date: 12/10/2024
+* Course #/Section: IS3050-001
+* Semester/Year: Fall 2024
+* Brief Description of the assignment: Final Project for the IS 3050 Web Development w/ .NET
+
+* Brief Description of what this module does: This module solves the "Minimum Number of Boxes to Build a Pyramid" problem from LeetCode.
+* Citations: https://leetcode.com/problems/building-boxes/
+             https://github.com/cole-crooks/crookscl_FinalProject 
+             https://chatgpt.com
+
+* Anything else that's relevant: team: Jack Driehaus (driehajl@mail.uc.edu), Derick Bellofatto (bellofdk@mail.uc.edu), and Cole Crooks (crookscl@mail.uc.edu).
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
